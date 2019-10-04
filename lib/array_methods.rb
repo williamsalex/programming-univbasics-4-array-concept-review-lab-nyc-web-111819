f1507604386d79ca6575b61d58b015b83d23ed9c
Alexander Williams <williams_alex@protonmail.com>
2019-10-04 20:32:48 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,9 @@
 def find_element_index(array, value_to_find)
   largestElement = 0
   array.each do |currElement|
-    
+    if array[currElement] > largestElement
+      largestElement = array[currElement]
+  return largestElement
 end
 
 def find_max_value(array)
